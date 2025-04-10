74397c6a6189316457439000e980729cffabe48b
Abhishek <abhishek9304302981@gmail.com>
2025-04-10 11:54:35 +0530

    create addProduct()

---
+++ src/main/java/io/github/abhi1127/shopkart/product/ProductService.java
@@ -0,0 +1,25 @@
+package io.github.abhi1127.shopkart.product;
+
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> productList;
+
+    public ProductService() {
+        productList = new LinkedList<>();
+    }
+
+    // CRUD = create , read , update ,delete
+//    CREATE
+    public Product addProduct(Product product){
+        productList.add(product);
+        return product;
+    }
+//    READ
+//    UPDATE
+//    DELETE
+}
