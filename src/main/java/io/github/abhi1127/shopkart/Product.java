53672df3faba2bd834d79ed9c5aad1dde1ea6126
Abhishek <abhishek9304302981@gmail.com>
2025-04-10 10:15:32 +0530

    create Product POJO

---
+++ src/main/java/io/github/abhi1127/shopkart/Product.java
@@ -0,0 +1,75 @@
+package io.github.abhi1127.shopkart;
+
+import java.util.Objects;
+
+public class Product {
+    private long id;
+    private String name;
+    private double price;
+    private String description;
+
+    public Product(){
+
+    }
+
+    public Product(long id, String name, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Double.compare(price, product.price) == 0 && Objects.equals(name, product.name) && Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
